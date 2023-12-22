74e6285b34f84479f67c7511e033016ca0bcfc1b
Mehmet Albayrak <albayrakmehmetdeu@gmail.com>
2023-12-22 05:59:25 +0300

    Product List displayed on screen with table

--- src/layouts/Categories.jsx
+++ src/layouts/Categories.jsx
@@ -1,5 +1,14 @@
-//console.log(5)
-//console.log(5)
-//console.log(5)
+import React from "react";
+import { Menu } from "semantic-ui-react";
 
-
+export default function Categories() {
+  return (
+    <div>
+      <Menu pointing vertical>
+        <Menu.Item name="home" />
+        <Menu.Item name="messages" />
+        <Menu.Item name="friends" />
+      </Menu>
+    </div>
+  );
+}
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Button, Icon, Label } from 'semantic-ui-react'
+import React from "react";
+import Categories from "./Categories";
+import ProductList from "../pages/ProductList";
+import { Grid } from "semantic-ui-react";
 
 export default function Dashboard() {
   return (
     <div>
-        
-  </div>
-  )
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Categories />
+          </Grid.Column>
+
+          <Grid.Column width={12}>
+            <ProductList />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 }
